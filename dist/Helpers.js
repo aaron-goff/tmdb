33aeab26538cf3f0785cf676201070ceedbb1832
Aaron Goff <ajgoff@gmail.com>
2025-06-14 11:00:46 -0400

    Fix files

--- dist/Helpers.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.standardScoring = void 0;
-exports.standardScoring = [5, 4, 3, 2, 1];
--- dist/data/serieses.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.seriesData = void 0;
-const Series_1 = require("./Series1/Series");
-const Series_2 = require("./Series2/Series");
-exports.seriesData = [Series_1.series1, Series_2.series2];
